e1fae2ba9c5eac67a4cd74d184b08bf083bd7281
hrsma2i <hrs.ma2i@gmail.com>
2021-03-14 17:32:34 +0900

    Unzip downloaded stuff from kaggle.

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,4 +3,13 @@ REPOSITORY_ROOT="$(
     cd "$(dirname "$(dirname "$0")")"
     pwd
 )"
-kaggle competitions download -c imaterialist-fashion-2019-FGVC6 -p "$REPOSITORY_ROOT"/raw
+
+RAW_DIR="$REPOSITORY_ROOT"/raw
+ZIP_FILENAME=imaterialist-fashion-2019-FGVC6.zip
+ZIP_FILE="$RAW_DIR"/$ZIP_FILENAME
+
+if [ ! -e "$ZIP_FILE" ]; then
+    kaggle competitions download -c imaterialist-fashion-2019-FGVC6 -p "$RAW_DIR"
+fi
+
+unzip "$ZIP_FILE" -d "$RAW_DIR"
